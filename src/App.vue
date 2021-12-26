ce06e3a6e7a7fec2be7e712051b17cba430a78af
Martin K <martinkz@users.noreply.github.com>
2021-12-26 18:59:45 +0000

    Update styles and reorganise

--- src/App.vue
+++ src/App.vue
@@ -23,3 +23,9 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+  .v-application {
+    background: #dddddd !important;
+  }
+</style>
--- src/components/CostsSection.vue
+++ src/components/CostsSection.vue
@@ -1,12 +1,11 @@
 <template>
-  <v-container>
-    
+  <v-container class="calculator-wrap">
     <v-sheet color="white">
       <!-- <v-container> -->
         <!-- <v-select :items="brands" outlined label="Radio brand" @change="selectChange($event)" v-model="currentBrand"></v-select> -->
-        <v-tabs v-model="radioBrandTab" background-color="deep-purple" fixed-tabs dark>
-          <v-tab v-for="item in radioBrands" :key="item">
-            {{ item }}
+        <v-tabs height="65px" class="radio-tabs" v-model="radioBrandTab" background-color="deep-purple" fixed-tabs dark>
+          <v-tab v-for="(item, idx) in radioBrands" :key="item">
+            <img class="radio-logo" :src="'img/'+radioLogos[idx]" alt="">
           </v-tab>
         </v-tabs>
       <!-- </v-container> -->
@@ -23,23 +22,16 @@
         <!-- </template> -->
       </v-sheet>
 
-      <v-tabs-items v-model="tab">
+      <v-tabs-items class="tabs" v-model="tab">
         <v-tab-item>
           <v-card flat>
             <!-- <v-card-text v-text="text"></v-card-text> -->
             <v-container>
+              <h2>Competition page</h2>
               <!-- <v-select :items="regions" filled label="Region" @change="selectChange($event)" v-model="currentRegion"></v-select> -->
               <v-select :items="regions" filled label="Regions" multiple v-model="currentRegions"></v-select>
               <v-select :items="pageTypes" filled label="Page Type" v-model="currentPageType"></v-select>
-            </v-container>
-            <v-container class="summary" v-if="getDataForCurrentRegions">
-              <h2>Summary for the selected regions</h2>
-              <h3>Monthly Home Page Views: {{ getDataForCurrentRegions['Monthly Home Page Views'] }}</h3> 
-              <h3>Daily Home Page Views: {{ getDataForCurrentRegions['Daily Home Page Views'] }}</h3> 
-              <h3>Daily Cost: £{{ getDataForCurrentRegions['Daily Cost'].toFixed(2) }}</h3> 
-              <h3>7 day cost: £{{ getDataForCurrentRegions['7 day cost'].toFixed(2) }}</h3> 
-              <h3 v-if="getDataForCurrentRegions['UK Monthly full site views']">UK Monthly full site views: {{ getDataForCurrentRegions['UK Monthly full site views'] }}</h3> 
-              <h3 v-if="getDataForCurrentRegions['UK Monthly users']">UK Monthly users: {{ getDataForCurrentRegions['UK Monthly users'] }}</h3> 
+              <v-text-field label="Campaign Days" type="number" filled v-model="pageDays"/>
             </v-container>
           </v-card>
         </v-tab-item>
@@ -47,17 +39,36 @@
         <v-tab-item>
           <v-container>
             <h2>CRM</h2>
+            <v-select :items="regions" filled label="Regions" multiple v-model="crmRegions"></v-select>
           </v-container>
         </v-tab-item>
         
         <v-tab-item>
           <v-container>
             <h2>Video</h2>
+            <v-slider
+              v-model="videoViews"
+              label="Video views"
+              thumb-color="#ff5252"
+              thumb-label="always"
+              max="10000"
+              step="100"
+              thumb-size="36"
+            ></v-slider>
           </v-container>
         </v-tab-item>
       </v-tabs-items>
     </v-card>
 
+    <v-container class="summary" v-if="getDataForCurrentRegions">
+      <h2>Summary for the selected regions</h2>
+      <h3>Monthly Home Page Views: {{ getDataForCurrentRegions['Monthly Home Page Views'] }}</h3> 
+      <h3>Daily Home Page Views: {{ getDataForCurrentRegions['Daily Home Page Views'] }}</h3> 
+      <h3>Daily Cost: £{{ getDataForCurrentRegions['Daily Cost'].toFixed(2) }}</h3> 
+      <h3>7 day cost: £{{ getDataForCurrentRegions['7 day cost'].toFixed(2) }}</h3> 
+      <h3 v-if="getDataForCurrentRegions['UK Monthly full site views']">UK Monthly full site views: {{ getDataForCurrentRegions['UK Monthly full site views'] }}</h3> 
+      <h3 v-if="getDataForCurrentRegions['UK Monthly users']">UK Monthly users: {{ getDataForCurrentRegions['UK Monthly users'] }}</h3> 
+    </v-container>
   </v-container>
 </template>
 
@@ -67,6 +78,7 @@
   let brandList = [];
   let pageTypes = ["Enhanced Competition Page", "Budget Competition Page"];
   let radioBrands = ['Heart', 'Capital', 'Smooth', 'CapitalXTRA', 'Classic', 'RadioX', 'LBC', 'Gold'];
+  let radioLogos = ['Heart_White_Nostrap.svg', 'Capital_White.png', 'smooth-white.png', 'CapitalXtra_White.png', 'Classic_white_min.svg', 'RadioX_Landscape_White.svg', 'LBC_White_Strap.svg', 'Gold.svg'];
 
   export default {
 
@@ -74,6 +86,7 @@
         // currentBrand: 'Heart',
         // currentRegion: undefined,
         currentRegions: [],
+        crmRegions: [],
         pageTypes: pageTypes,
         currentPageType: undefined,
         brands: brandList,
@@ -81,6 +94,9 @@
         tab: null,
         radioBrandTab: 0,
         radioBrands: radioBrands,
+        radioLogos: radioLogos,
+        pageDays: 7,
+        videoViews: 5000,
         items2: [ 'Pages', 'CRM', "Video" ],
     }),
 
@@ -144,14 +160,37 @@
           });
           brandList.push(...Object.keys(sheetData));
           this.currentRegions.push("NETWORK");
+          this.crmRegions.push("NETWORK");
       });
     },
 
   }
 </script>
 
-<style scoped>
-  /* .container {
-      max-width: 1000px;
-  } */
+<style lang="scss" scoped>
+
+  .calculator-wrap {
+    max-width: 960px;
+  }
+
+  .tabs h2 {
+    margin-bottom: 1rem;
+  }
+
+  .radio-tabs {
+    // Doesn't work for some reason
+    // .v-tabs-bar {
+    //   height: 55px !important;
+    // }
+    
+  }
+
+  .v-tab--active {
+      background: #3b0f89;
+  }
+
+  .radio-logo {
+    max-width: 90px;
+    max-height: 36px;
+  }
 </style>
